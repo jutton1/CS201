eb4d99058e0e97258c2ad9ce392e92af9ae6f1d8
jutton1 <55819679+jutton1@users.noreply.github.com>
2023-01-13 14:28:34 -0500

    Add files via upload

--- testing/tutoring.java
+++ testing/tutoring.java
@@ -1,23 +1,14 @@
 public class tutoring {
-    public String main(String paragraph, String find, String replace) {
 
-        String returnString = new String();
-        int wordLength = find.length();
-        for (int i = 0; i < paragraph.length(); i++) {
-            if (paragraph.substring(i, i + wordLength).compareTo(find) == 0) {
-                returnString += replace;
-                i += wordLength;
-            }
-            else {
-                returnString += paragraph.charAt(i);
-            }
-        }
+    static int pwCounter;
 
-        return returnString;
+    public void main() {
+
+
+        String testing = new String("A");
+        String testing2 = "A";
 
     }
-
-
 }
 
 
